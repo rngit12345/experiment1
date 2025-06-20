7488931d62cbe133eeefe15389941527f9d86b23
rngit12345 <105807001+rngit12345@users.noreply.github.com>
2025-06-20 09:55:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import pickle
+
+savedir = './results/'
+savefile = 'values.pkl'
+
 a = [1,2,3,4]
 for val in a:
 	print(val)
+
+# Saving
+
+with open(savedir+savefile, 'wb') as f:
+    pickle.dump(a, f)
+
